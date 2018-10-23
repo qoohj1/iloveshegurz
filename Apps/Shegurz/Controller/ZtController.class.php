5ce13cae6d17d52ac0d3fc2036854bf367414629
qoohj1 <qoohj@qq.com>
2018-10-24 03:02:44 +0800

    up

--- Apps/Shegurz/Controller/ZtController.class.php
+++ Apps/Shegurz/Controller/ZtController.class.php
@@ -12,31 +12,63 @@ class ZtController extends Controller {
     }
 
     public function lottery() {
-      $prize_arr = M('lottery')->select();
-      foreach ($prize_arr as $key => $val) {
-        $arr[$val['id']] = $val['v'];
-      }
-      $rid = $this->get_rand($arr); //根据概率获取奖项id
-      var_dump($rid);
+      $condition['um'] = I('um');
+      if($condition['um']){
+        $result = M('um')->where($condition)->find();
+        if($result&&$result['prize']){
+          jsonReturn(1,error,'您已抽奖');
+        }
+        $prize_arr = M('lottery')->select();
+        // var_dump($prize_arr);
+        if(empty($prize_arr)){
+          jsonReturn(1,error,'奖池已空');
 
-      $res['yes'] = $prize_arr[$rid-1]['prize']; //中奖项
-      var_dump($res['yes']);
-      unset($prize_arr[$rid-1]); //将中奖项从数组中剔除，剩下未中奖项
-      shuffle($prize_arr); //打乱数组顺序
-      for($i=0;$i<count($prize_arr);$i++){
-          $pr[] = $prize_arr[$i]['prize'];
+        }else{
+          foreach ($prize_arr as $key => $val) {
+            $arr[$val['id']] = $val['v'];
+          }
+          // shuffle($prize_arr); //打乱数组顺序
+          // var_dump($arr);
+          $rid = $this->get_rand($arr); //根据概率获取奖项id
+          // var_dump($rid);
+          $data = M('lottery')->where('id='.$rid)->find();
+          $lottery = $data['prize'];
+          $result1 = M('um')->where(array('um'=>$condition['um']))->save(array('prize'=>$lottery));
+          // var_dump($result1);
+          // $res['yesid'] = $prize_arr[$rid-1]['id']; //中奖项
+          // var_dump($res['yesid']);
+          $result2 = M('lottery')->where('id='.$rid)->delete();
+          if($result1&&$result2){
+            jsonReturn(0,success,$lottery);
+          }else{
+            jsonReturn(1,error,'操作失败');
+          }
+
+        }
+
+      }else{
+        jsonReturn(1,error,'请先输入um号');
       }
-      $res['no'] = $pr;
-      var_dump($res['no']);
 
     }
+
+    // public function delLottery($rid) {
+    //   return $result;
+    // }
+    //
 
     public function verifyum() {
       $condition['um'] = I('um');
 
-      $result = M('um')->where($condition)->select();
+      $result = M('um')->where($condition)->find();
+      // var_dump($result);
       if($result){
-          jsonReturn(0,success, $result);
+          if($result->prize){
+            jsonReturn(1,error,'每人抽奖1次');
+          }else{
+            jsonReturn(0,success, $result);
+          }
+          // if($result)
       }else{
           jsonReturn(1,error,'请输入正确的um号');
       }
@@ -46,6 +78,7 @@ class ZtController extends Controller {
         $result = '';
         //概率数组的总概率精度
         $proSum = array_sum($proArr);
+        // var_dump($proArr);
         //概率数组循环
         foreach ($proArr as $key => $proCur) {
             $randNum = mt_rand(1, $proSum);
